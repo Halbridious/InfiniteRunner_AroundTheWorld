0b7b24ff8e1e5e1844880f654e59ad3a33306a7b
Jacob Hamberg <halbridious@gmail.com>
2017-09-07 14:21:43 -0400

    Copied over intial Unity Project
    Should now include all of the stuff I did in wk1 for pushing to github.

---
+++ Assets/Scripts/Player_Mover.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * PLAYER MOVER is a BEHAVIOUR that allows the player to "Jump"
+ * Players jump using the JUMP key
+ * Players can NOT move during a jump!
+ *  >Currently, moving is done on the sphere.  
+ *    
+ *  TODO:
+ *  >Acceleration on jump
+ *  >Impliment either 1-press or timed jump system
+ **/
+
+public class Player_Mover : MonoBehaviour {
+
+    [SerializeField]
+    float jumpPower = 50f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        //If the jump button is pressed ("Jump" is assigned in program properties)
+        if(Input.GetAxisRaw("Jump") > 0) {
+            
+            //accelerate into the jump
+            transform.position += new Vector3(0, 0, jumpPower) * Time.deltaTime;
+            
+            //Once they've reached the highest point of the jump, cap it
+            if( transform.position.z >= 30 ) transform.position = new Vector3(0, 0, 30);
+
+        } else {//if the button is released
+
+            //accelerate back towards neutral position
+            if(transform.position.z > 26) transform.position -= new Vector3(0, 0, jumpPower) * Time.deltaTime;
+
+            //once in or past neutral position, set exactly to neutral
+            if( transform.position.z <= 26 ) transform.position = new Vector3(0, 0, 26);
+        }
+	}
+}
---
+++ Assets/Scripts/Sphere_Mover.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * SPHERE MOVER is a BEHAVIOUR that simulates the movement laterally for the player
+ * Players will change lanes by hitting left or right, giving the illusion of sliding side to side.
+ * Sphere also constantly rotates directly "towards" the player
+ *  >Player changing lanes will, in turn, change angle of approaching obstacles
+ * 
+ * TODO:
+ * accelerate and decelerate lateral movement
+ **/
+
+
+public class Sphere_Mover : MonoBehaviour {
+
+    [SerializeField]
+    private float horSpeed = 20f;
+
+    [SerializeField]
+    private float verSpeed = 20f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.RotateAround(Vector3.zero, new Vector3(1, 0, 0), verSpeed * Time.deltaTime);
+
+        float move = Input.GetAxisRaw("Horizontal");
+
+        if (move > 0 ) {
+
+            transform.RotateAround(Vector3.zero, new Vector3(0, 1, 0), horSpeed * Time.deltaTime);
+
+        } else if (move < 0 ) {
+            transform.RotateAround(Vector3.zero, new Vector3(0, 1, 0), -horSpeed * Time.deltaTime);
+        } else {
+            //no input, just calculate friction
+        }
+
+	}
+}
